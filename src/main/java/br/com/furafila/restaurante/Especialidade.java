5dbeb958e6aaa5a7ef50324339cb946dfe9928d1
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-01-18 12:34:52 -0300

    criado especialidade restaurante

---
+++ src/main/java/br/com/furafila/restaurante/Especialidade.java
@@ -0,0 +1,12 @@
+package br.com.furafila.restaurante;
+
+public enum Especialidade {
+    PIZZARIA,
+    CHINESA,
+    ITALIANA,
+    JAPONESA,
+    ARABE,
+    MEXICANA,
+    CHURRASCARIA,
+    FASTFOOD
+}
