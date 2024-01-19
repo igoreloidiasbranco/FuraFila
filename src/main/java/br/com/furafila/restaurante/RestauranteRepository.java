e226294507dcba6fe9f062917975db9223352caa
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-01-19 17:22:43 -0300

    criado RestauranteRepository

---
+++ src/main/java/br/com/furafila/restaurante/RestauranteRepository.java
@@ -0,0 +1,6 @@
+package br.com.furafila.restaurante;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestauranteRepository extends JpaRepository<Restaurante,Long> {
+}
