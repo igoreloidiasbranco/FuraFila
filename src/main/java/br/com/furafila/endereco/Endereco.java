4739e4617899826c7252bb77545e0a20df3ee514
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-01-19 17:17:10 -0300

    criado classe Endereco

--- src/main/java/br/com/furafila/endereco/Endereco.java
+++ src/main/java/br/com/furafila/endereco/Endereco.java
@@ -1,6 +1,30 @@
 package br.com.furafila.endereco;
 
-public record Endereco(String logradouro, String numero, String complemento,
-                       String bairro, String cep, String cidade, String uf) {
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    String logradouro;
+    String numero;
+    String complemento;
+    String bairro;
+    String cep;
+    String cidade;
+    String uf;
+
+    public Endereco(EnderecoDTO enderecoDTO) {
+        this.logradouro = enderecoDTO.logradouro();
+        this.numero = enderecoDTO.numero();
+        this.complemento = enderecoDTO.complemento();
+        this.bairro = enderecoDTO.bairro();
+        this.cep = enderecoDTO.cep();
+        this.cidade = enderecoDTO.cidade();
+        this.uf = enderecoDTO.uf();
+    }
 }
