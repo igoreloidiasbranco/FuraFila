fbc14bd523573c26bc71dc3517d96ba9525e1703
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-01-19 17:15:56 -0300

    renomeado ClienteDTO

--- src/main/java/br/com/furafila/cliente/Cliente.java
+++
@@ -1,7 +0,0 @@
-package br.com.furafila.cliente;
-
-import br.com.furafila.endereco.Endereco;
-
-public record Cliente(String nome, String cpf, String telefone, String email,
-                      String datanascimento, Endereco endereco) {
-}
---
+++ src/main/java/br/com/furafila/cliente/ClienteDTO.java
@@ -0,0 +1,7 @@
+package br.com.furafila.cliente;
+
+import br.com.furafila.endereco.EnderecoDTO;
+
+public record ClienteDTO(String nome, String cpf, String telefone, String email,
+                         String datanascimento, EnderecoDTO enderecoDTO) {
+}
