97f32f7f64738ac0949f32fecdaaa491bf36fab0
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-01-19 17:19:15 -0300

    criado classe Restaurante

--- src/main/java/br/com/furafila/restaurante/Restaurante.java
+++ src/main/java/br/com/furafila/restaurante/Restaurante.java
@@ -1,6 +1,37 @@
 package br.com.furafila.restaurante;
 
 import br.com.furafila.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record Restaurante(String nome, String email, Especialidade especialidade, Endereco endereco) {
+@Table(name = "restaurantes")
+@Entity(name = "Restaurante")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Restaurante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+    private Integer capacidade;
+    @Embedded
+    private Endereco endereco;
+
+    public Restaurante(RestauranteDTO dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.especialidade = dados.especialidade();
+        this.capacidade = dados.capacidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
 }
