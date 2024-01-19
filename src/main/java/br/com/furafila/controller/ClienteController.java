3b7781447671674584e773c123827c858d5e42b9
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-01-19 17:15:14 -0300

    renomeado ClienteDTO

--- src/main/java/br/com/furafila/controller/ClienteController.java
+++ src/main/java/br/com/furafila/controller/ClienteController.java
@@ -1,6 +1,6 @@
 package br.com.furafila.controller;
 
-import br.com.furafila.cliente.Cliente;
+import br.com.furafila.cliente.ClienteDTO;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("clientes")
 public class ClienteController {
     @PostMapping
-    public void cadastrar (@RequestBody Cliente dadosCliente ) {
-        System.out.println(dadosCliente);
+    public void cadastrar (@RequestBody ClienteDTO dadosClienteDTO) {
+        System.out.println(dadosClienteDTO);
     }
 }
