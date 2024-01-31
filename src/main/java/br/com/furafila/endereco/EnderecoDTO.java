d13e7e60ea4feb9e29ef4d879c42f3e3190dc69b
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-01-31 20:18:26 -0300

    validações do endereço

--- src/main/java/br/com/furafila/endereco/EnderecoDTO.java
+++ src/main/java/br/com/furafila/endereco/EnderecoDTO.java
@@ -1,6 +1,22 @@
 package br.com.furafila.endereco;
 
-public record EnderecoDTO(String logradouro, String numero, String complemento,
-                          String bairro, String cep, String cidade, String uf) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record EnderecoDTO(
+        @NotBlank
+        String logradouro,
+        @NotBlank
+        String numero,
+        String complemento,
+        @NotBlank
+        String bairro,
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String cep,
+        @NotBlank
+        String cidade,
+        @NotBlank
+        String uf) {
 
 }
