0c98ff4435f06dccc8bcd450c34dad7c2a59197e
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-01-31 20:22:11 -0300

    validações dos dados do restaurante.

--- src/main/java/br/com/furafila/restaurante/RestauranteDTO.java
+++ src/main/java/br/com/furafila/restaurante/RestauranteDTO.java
@@ -1,7 +1,32 @@
 package br.com.furafila.restaurante;
 
 import br.com.furafila.endereco.EnderecoDTO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record RestauranteDTO(String nome, String cnpj, String email, String telefone,
-                             Especialidade especialidade, Integer capacidade, EnderecoDTO endereco) {
+public record RestauranteDTO(
+        @NotBlank
+        String nome,
+
+        @NotBlank
+        String cnpj,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String telefone,
+
+        @NotNull
+        Especialidade especialidade,
+
+        @NotNull
+        Integer capacidade,
+
+        @NotNull
+        @Valid
+        EnderecoDTO endereco) {
 }
