10de372d670e287a2ae19032a193328f992b6ce2
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-02-02 20:24:13 -0300

    paginação do método listar

--- src/main/java/br/com/furafila/controller/RestauranteController.java
+++ src/main/java/br/com/furafila/controller/RestauranteController.java
@@ -6,6 +6,9 @@ import br.com.furafila.restaurante.RestauranteDTO;
 import br.com.furafila.restaurante.RestauranteRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +27,7 @@ public class RestauranteController {
     }
 
     @GetMapping
-    public List<ListagemRestaurantesDTO> listar() {
-        return repository.findAll().stream().map(ListagemRestaurantesDTO::new).toList();
+    public Page<ListagemRestaurantesDTO> listar(@PageableDefault(size = 5, sort = {"nome"} ) Pageable paginacao) {
+        return repository.findAll(paginacao).map(ListagemRestaurantesDTO::new);
     }
 }
