6b3d004f71738d3369b60dced9bca32bfef3c003
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-02-01 23:35:00 -0300

    criação do método GET listar

--- src/main/java/br/com/furafila/controller/RestauranteController.java
+++ src/main/java/br/com/furafila/controller/RestauranteController.java
@@ -1,24 +1,30 @@
 package br.com.furafila.controller;
 
+import br.com.furafila.restaurante.ListagemRestaurantesDTO;
 import br.com.furafila.restaurante.Restaurante;
 import br.com.furafila.restaurante.RestauranteDTO;
 import br.com.furafila.restaurante.RestauranteRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("restaurantes")
 public class RestauranteController {
     @Autowired
     RestauranteRepository repository;
+
     @PostMapping
     @Transactional
     public void cadastrar(@RequestBody @Valid RestauranteDTO restauranteDTO) {
         repository.save(new Restaurante(restauranteDTO));
+    }
+
+    @GetMapping
+    public List<ListagemRestaurantesDTO> listar() {
+        return repository.findAll().stream().map(ListagemRestaurantesDTO::new).toList();
     }
 }
