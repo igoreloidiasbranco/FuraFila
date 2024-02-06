06bc2cb948506bdd53fa48fbe0e060769b171129
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-02-06 20:32:54 -0300

    adicionado BeanValidation @Email

--- src/main/java/br/com/furafila/restaurante/DadosAtualizarRestauranteDTO.java
+++ src/main/java/br/com/furafila/restaurante/DadosAtualizarRestauranteDTO.java
@@ -1,11 +1,13 @@
 package br.com.furafila.restaurante;
 
 import br.com.furafila.endereco.EnderecoDTO;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 
 public record DadosAtualizarRestauranteDTO(
         @NotNull
         Long id,
+        @Email
         String email,
         String telefone,
         Especialidade especialidade,
