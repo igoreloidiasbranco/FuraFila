b2d5447c6b9c9f1e8bf369542671cf787557a8f0
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-02-07 13:17:51 -0300

    listagem por ativo=true

--- src/main/java/br/com/furafila/restaurante/RestauranteRepository.java
+++ src/main/java/br/com/furafila/restaurante/RestauranteRepository.java
@@ -1,6 +1,16 @@
 package br.com.furafila.restaurante;
 
+
+
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface RestauranteRepository extends JpaRepository<Restaurante,Long> {
+
+
+    Page<Restaurante> findAllByAtivoTrue(Pageable paginacao);
 }
