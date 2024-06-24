1fc593485a07cefe44eba92e854cee68b69737de
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-24 17:15:08 -0300

    add atributo tokenService

--- src/main/java/br/com/furafila/controller/AutenticacaoController.java
+++ src/main/java/br/com/furafila/controller/AutenticacaoController.java
@@ -2,6 +2,9 @@ package br.com.furafila.controller;
 
 
 import br.com.furafila.domain.usuario.DadosAutenticacaoDTO;
+import br.com.furafila.domain.usuario.Usuario;
+import br.com.furafila.security.DadosTokenJWT;
+import br.com.furafila.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +22,17 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacaoDTO dadosAutenticacaoDTO) {
 
-        var token = new UsernamePasswordAuthenticationToken(dadosAutenticacaoDTO.login(), dadosAutenticacaoDTO.senha());
-        var authentication = authenticationManager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dadosAutenticacaoDTO.login(), dadosAutenticacaoDTO.senha());
+        var authentication = authenticationManager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 }
