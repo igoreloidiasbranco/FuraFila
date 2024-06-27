233d3d151b4b83326ac3a4ee1fc333909022ef62
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-27 18:06:31 -0300

    add interface ReservaRepository

---
+++ src/main/java/br/com/furafila/domain/reserva/ReservaRepository.java
@@ -0,0 +1,6 @@
+package br.com.furafila.domain.reserva;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+}
