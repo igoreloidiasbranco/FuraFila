b3adfaf23c3eb1a23348ae6a40cdb56f627897a5
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-27 18:07:08 -0300

    add migration create table reservas

---
+++ src/main/resources/db/migration/V6__create-table-reservas.sql
@@ -0,0 +1,11 @@
+create table reservas(
+
+    id bigint not null auto_increment,
+    restaurante_id bigint not null,
+    cliente_id bigint not null,
+    data datetime not null,
+
+    primary key(id),
+    constraint fk_reservas_restaurante_id foreign key(restaurante_id) references restaurantes(id),
+    constraint fk_reservas_cliente_id foreign key(cliente_id) references clientes(id)
+)
