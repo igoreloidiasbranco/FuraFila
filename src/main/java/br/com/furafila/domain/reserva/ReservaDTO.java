67099e6ade34116d5de67ed9e33bb28159bbea6b
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-07-08 17:22:07 -0300

    add atributo qtde

--- src/main/java/br/com/furafila/domain/reserva/ReservaDTO.java
+++ src/main/java/br/com/furafila/domain/reserva/ReservaDTO.java
@@ -1,6 +1,8 @@
 package br.com.furafila.domain.reserva;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
@@ -13,7 +15,12 @@ public record ReservaDTO(
         Long idCliente,
 
         @NotNull
+        @Min(value = 1, message = "A quantidade de pessoas para a reserva não pode ser menor do que 1")
+        Integer qtde,
+
+        @NotNull
         @Future
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
         LocalDateTime data
 ) {
 }
