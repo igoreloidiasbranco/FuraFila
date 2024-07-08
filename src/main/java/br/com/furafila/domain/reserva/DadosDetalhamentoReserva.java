8d1425b843fcc43b8cf6c544bd1d988821085ac3
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-07-08 17:20:24 -0300

    add atributo qtde

--- src/main/java/br/com/furafila/domain/reserva/DadosDetalhamentoReserva.java
+++ src/main/java/br/com/furafila/domain/reserva/DadosDetalhamentoReserva.java
@@ -1,6 +1,15 @@
 package br.com.furafila.domain.reserva;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
-public record DadosDetalhamentoReserva(Long id, Long idRestaurante, Long idCliente, LocalDateTime data) {
+public record DadosDetalhamentoReserva(
+        Long id,
+        Long idRestaurante,
+        Long idCliente,
+        Integer qtde,
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+        LocalDateTime data
+) {
 }
