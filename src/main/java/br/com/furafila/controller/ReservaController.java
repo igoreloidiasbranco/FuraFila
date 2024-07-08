d52697df40edf28e689cf2ac9e93cddf52f40313
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-07-08 17:24:16 -0300

    add atributo reservaService

--- src/main/java/br/com/furafila/controller/ReservaController.java
+++ src/main/java/br/com/furafila/controller/ReservaController.java
@@ -2,8 +2,10 @@ package br.com.furafila.controller;
 
 import br.com.furafila.domain.reserva.DadosDetalhamentoReserva;
 import br.com.furafila.domain.reserva.ReservaDTO;
+import br.com.furafila.service.ReservaService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,10 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/reservas")
 public class ReservaController {
 
+    @Autowired
+    private ReservaService reservaService;
+
     @PostMapping
     @Transactional
     public ResponseEntity reservar(@RequestBody @Valid ReservaDTO reservaDTO) {
-        System.out.println(reservaDTO);
-        return ResponseEntity.ok(new DadosDetalhamentoReserva(null, null, null, null));
+
+        reservaService.reservar(reservaDTO);
+        return ResponseEntity.ok(new DadosDetalhamentoReserva(null, null, null, null, null));
     }
 }
