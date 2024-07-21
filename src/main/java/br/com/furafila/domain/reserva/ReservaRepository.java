6a070452c7f0abc378cf33de8a43c356da2074b5
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-07-21 13:37:28 -0300

    add método existsByClienteIdAndDataBetween

--- src/main/java/br/com/furafila/domain/reserva/ReservaRepository.java
+++ src/main/java/br/com/furafila/domain/reserva/ReservaRepository.java
@@ -2,5 +2,10 @@ package br.com.furafila.domain.reserva;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+
+    Boolean existsByClienteIdAndDataBetween(Long idCliente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
+
 }
