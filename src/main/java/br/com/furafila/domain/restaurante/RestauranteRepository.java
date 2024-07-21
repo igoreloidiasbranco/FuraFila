d92758835da25b90b679f2048497542e88fb1b02
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-07-21 13:40:30 -0300

    add escolherRestauranteAleatorioNaData

--- src/main/java/br/com/furafila/domain/restaurante/RestauranteRepository.java
+++ src/main/java/br/com/furafila/domain/restaurante/RestauranteRepository.java
@@ -1,16 +1,39 @@
 package br.com.furafila.domain.restaurante;
 
-
-
-
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import java.time.LocalDateTime;
 
 public interface RestauranteRepository extends JpaRepository<Restaurante,Long> {
 
 
     Page<Restaurante> findAllByAtivoTrue(Pageable paginacao);
+
+   @Query("""
+           select r from Restaurante r
+           where
+           r.ativo = true
+           and
+           r.especialidade = :especialidade
+           and
+           r.id not in(
+                select rsv.restaurante.id from Reserva rsv
+                where
+                rsv.data = :data
+           )
+           order by rand()
+           limit 1
+           """)
+    Restaurante escolherRestauranteAleatorioNaData(Especialidade especialidade, LocalDateTime data);
+
+
+   @Query("""
+           select r.ativo
+           from Restaurante r
+           where
+           r.id = :id
+           """)
+   Boolean findAtivoById(Long id);
 }
